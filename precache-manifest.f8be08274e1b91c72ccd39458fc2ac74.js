7bdf4611fa27704e52d201d01f3d410cb39ae5e2
Vitaliy Petrenko <lpdev.vitaliy@gmail.com>
2019-12-13 04:36:55 +0200

    Updates

---
+++ precache-manifest.f8be08274e1b91c72ccd39458fc2ac74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b6cf00ed67913211731411a6186fbb",
+    "url": "/squares/index.html"
+  },
+  {
+    "revision": "0b3b43280ff97c85e93e",
+    "url": "/squares/static/css/main.482b8cc5.chunk.css"
+  },
+  {
+    "revision": "95b766aea75890e3299c",
+    "url": "/squares/static/js/2.7603fc90.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/squares/static/js/2.7603fc90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b3b43280ff97c85e93e",
+    "url": "/squares/static/js/main.eaa8378f.chunk.js"
+  },
+  {
+    "revision": "6ed4a9b4dfe8127323af",
+    "url": "/squares/static/js/runtime-main.93103d08.js"
+  }
+]);
