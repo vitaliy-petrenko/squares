0f55ae3d7b6c362b90674e52ff2ff30482dc3f31
Vitaliy Petrenko <lpdev.vitaliy@gmail.com>
2019-12-17 20:00:36 +0200

    Updates

---
+++ precache-manifest.47850eb476cfd5bfb769594f3a48e2a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f170b30c95fcaa4282e55c95305b6d6d",
+    "url": "/squares/index.html"
+  },
+  {
+    "revision": "21bbb5d411c88c895492",
+    "url": "/squares/static/css/main.fd970db6.chunk.css"
+  },
+  {
+    "revision": "0ff3f65413be6b0426ff",
+    "url": "/squares/static/js/2.a70101fd.chunk.js"
+  },
+  {
+    "revision": "1fde4a744ed936ec490428ffb6893eb8",
+    "url": "/squares/static/js/2.a70101fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21bbb5d411c88c895492",
+    "url": "/squares/static/js/main.3441ad7d.chunk.js"
+  },
+  {
+    "revision": "6ed4a9b4dfe8127323af",
+    "url": "/squares/static/js/runtime-main.93103d08.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/squares/precache-manifest.30963d3a3d79218d520bc4e7203894e6.js"
+  "/squares/precache-manifest.47850eb476cfd5bfb769594f3a48e2a9.js"
 );
 
 self.addEventListener('message', (event) => {
